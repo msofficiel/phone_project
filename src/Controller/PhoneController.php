4535bd2bb105012ca279361e0fb5dde65e96062d
msofficiel <msoumarabdelkerim@gmail.com>
2026-06-15 13:36:25 +0200

    Données dans MySQL, Controller nettoyé

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -15,23 +15,7 @@ final class PhoneController extends AbstractController
     #[Route('/phone', name: 'app_phone')]
     public function index(PhoneRepository $phoneRepository, EntityManagerInterface $em): Response
     {
-        if (count($phoneRepository->findAll()) === 0) {
-            $phones = [
-                ['name' => 'iPhone 15 Pro', 'brand' => 'Apple', 'price' => 1299.99],
-                ['name' => 'Galaxy S24', 'brand' => 'Samsung', 'price' => 999.99],
-                ['name' => 'Pixel 8', 'brand' => 'Google', 'price' => 799.99],
-                ['name' => 'Xiaomi 14', 'brand' => 'Xiaomi', 'price' => 699.99],
-            ];
-
-            foreach ($phones as $data) {
-                $phone = new Phone();
-                $phone->setName($data['name']);
-                $phone->setBrand($data['brand']);
-                $phone->setPrice($data['price']);
-                $em->persist($phone);
-            }
-            $em->flush();
-        }
+       
 
         $phones = $phoneRepository->findAll();
         return $this->render('phone/index.html.twig', ['phones' => $phones]);
