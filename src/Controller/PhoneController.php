c52e0d2f003eac2e97bbdca038fbc5dcf8a43c5a
msofficiel <msoumarabdelkerim@gmail.com>
2026-06-15 12:54:53 +0200

    Amélioration design et ajout formulaire

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,9 +2,11 @@
 namespace App\Controller;
 
 use App\Entity\Phone;
+use App\Form\PhoneType;
 use App\Repository\PhoneRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -32,9 +34,28 @@ final class PhoneController extends AbstractController
         }
 
         $phones = $phoneRepository->findAll();
+        return $this->render('phone/index.html.twig', ['phones' => $phones]);
+    }
 
-        return $this->render('phone/index.html.twig', [
-            'phones' => $phones,
-        ]);
+    #[Route('/phone/new', name: 'app_phone_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $phone = new Phone();
+        $form = $this->createForm(PhoneType::class, $phone);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($phone);
+            $em->flush();
+            return $this->redirectToRoute('app_phone');
+        }
+
+        return $this->render('phone/new.html.twig', ['form' => $form]);
+    }
+
+    #[Route('/phone/{id}', name: 'app_phone_show')]
+    public function show(Phone $phone): Response
+    {
+        return $this->render('phone/show.html.twig', ['phone' => $phone]);
     }
 }
